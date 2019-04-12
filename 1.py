a42979f2949f0ae3f4d587419606c5ea60c771b4
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-04-12 13:53:12 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,43 @@
+#print(42 == 42)
+#print(42==99)
+#print(2!=5)
+#print(2!=2)
+#print('hello' == 'hello')
+#print('hello' == 'Hello')
+#print('dog' != 'cat')
+#print(True == True)
+#print(True != False)
+#print(42 == 42.0)
+#print(42 == '42')
+#print(42 < 100)
+#print(42 > 100)
+#eggCount = 42
+#print(eggCount <= 42)
+#my_age = 20
+#print(my_age >= 10)
+#print(True and True)
+#print(True and False)
+#print(False or True)
+#print(False or False)
+#print(not True)
+#print(not not not not True)
+#print((4 < 5) and (5 < 6))
+#print((4 < 5) and (9 < 6))
+#print((1==2) or (2==2))
+#name = input("username \n")
+#password = input("password \n")
+#if name == 'aakash':
+#
+#    print(f"Hello {name}")
+#if password == 'Padhiyar':
+#    print("access granted")
+#
+#else:
+#    print("wrong password.")
+
+
+
+
+
+
+
